24441033441a6d9d95c6d8d13cd95d6bb44f3a2e
Raúl Cátedra <raulcabe3003@gmail.com>
2021-02-10 16:38:21 +0100

    start dashboard.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if (isset($_GET['error'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Employee management V1</title>
-  <link rel="stylesheet" href="assets/css/main.css">
+  <link rel="stylesheet" href="assets/css/login.css">
   <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
 </head>
 
--- src/dashboard.php
+++ src/dashboard.php
@@ -1,1 +1,25 @@
 <!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/main.css">
+  <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+  <title>Dashborad</title>
+</head>
+
+<body>
+  <?php
+  require("../assets/html/header.html");
+  ?>
+  <section id="mainContent"></section>
+  <?php
+  require("../assets/html/footer.html");
+  ?>
+  <script src="../assets/js/index.js"></script>
+</body>
+
+</html>
