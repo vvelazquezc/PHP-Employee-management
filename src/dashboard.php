829bb38685550ccbbcb972532d585011c957c9ce
Raúl Cátedra <raulcabe3003@gmail.com>
2021-02-10 16:57:09 +0100

    fixe dashboard links path

--- src/dashboard.php
+++ src/dashboard.php
@@ -6,8 +6,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/css/main.css">
-  <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+  <link rel="stylesheet" href="../assets/css/main.css">
+  <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
   <title>Dashborad</title>
 </head>
 
