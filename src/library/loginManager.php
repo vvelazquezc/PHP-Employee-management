0719b7bf12475ae8853cabec760d61f1316b1059
iarrieta90 <irati_arrieta90@hotmail.com>
2021-02-11 10:06:04 +0100

    lifetime to 10min

--- src/library/loginManager.php
+++ src/library/loginManager.php
@@ -20,7 +20,7 @@ function saveSesion($user)
   $_SESSION['email'] = $user['email'];
   $_SESSION['name'] = $user['name'];
   $_SESSION['init'] = time();
-  $_SESSION['life'] = 5;
+  $_SESSION['life'] = 600;
 }
 
 function logOut()
