57e401e1ce62bcacf667d9f2f881ebd6f8d11f53
Raúl Cátedra <raulcabe3003@gmail.com>
2021-02-12 12:06:14 +0100

    change employee form names

--- src/employee.php
+++ src/employee.php
@@ -19,7 +19,7 @@ require("../assets/html/header.html");
       <div class="row g-3">
         <div class="col-sm-6">
           <label for="uName" class="form-label">First name</label>
-          <input type="text" class="form-control" id="uName" name="uName" placeholder="" value="<?= isset($employee) ? $employee['name'] : '' ?>" required="">
+          <input type="text" class="form-control" id="uName" name="name" placeholder="" value="<?= isset($employee) ? $employee['name'] : '' ?>" required="">
           <div class="invalid-feedback">
             Valid first name is required.
           </div>
@@ -27,51 +27,52 @@ require("../assets/html/header.html");
 
         <div class="col-sm-6">
           <label for="uLastName" class="form-label">Last name</label>
-          <input type="text" class="form-control" id="uLastName" name="uLastName" placeholder="" value="<?= isset($employee) ? $employee['lastName'] : '' ?>" required="">
+          <input type="text" class="form-control" id="uLastName" name="lastName" placeholder="" value="<?= isset($employee) ? $employee['lastName'] : '' ?>" required="">
         </div>
 
         <div class="col-sm-6">
           <label for="uEmail" class="form-label">Email</label>
-          <input type="email" class="form-control" id="uEmail" name="uEmail" value="<?= isset($employee) ? $employee['email'] : '' ?>" required="">
+          <input type="email" class="form-control" id="uEmail" name="email" value="<?= isset($employee) ? $employee['email'] : '' ?>" required="">
         </div>
 
         <div class="col-md-6">
           <label for="uGender" class="form-label">Gender</label><br>
-          <select class="form-select" id="uGender" name="uGender" required>
+          <select class="form-select" id="uGender" name="gender" required>
             <option value="man" <?= isset($employee) ? ($employee['gender'] == "man" ? "selected" : "") : '' ?>>Man</option>
             <option value="woman" <?= isset($employee) ? ($employee['gender'] == "woman" ? "selected" : "") : '' ?>>Woman</option>
             <option value="nobinary" <?= isset($employee) ? ($employee['gender'] == "nobinary" ? "selected" : "") : '' ?>>No binary</option>
+            <option value="" <?= isset($employee) ? ($employee['gender'] == "" ? "selected" : "") : '' ?>></option>
           </select>
         </div>
 
         <div class="col-md-6">
           <label for="uCity" class="form-label">City</label>
-          <input type="text" class="form-control" id="uCity" name="uCity" value="<?= isset($employee) ? $employee['city'] : '' ?>">
+          <input type="text" class="form-control" id="uCity" name="city" value="<?= isset($employee) ? $employee['city'] : '' ?>">
         </div>
 
         <div class="col-md-6">
           <label for="uAddress" class="form-label">Street Address</label>
-          <input type="text" class="form-control" id="uAddress" name="uAddress" required value="<?= isset($employee) ? $employee['streetAddress'] : '' ?>">
+          <input type="text" class="form-control" id="uAddress" name="address" required value="<?= isset($employee) ? $employee['streetAddress'] : '' ?>">
         </div>
 
         <div class="col-md-6">
           <label for="uState" class="form-label">State</label>
-          <input type="text" class="form-control" id="uState" name="uState" value="<?= isset($employee) ? $employee['state'] : '' ?>">
+          <input type="text" class="form-control" id="uState" name="state" value="<?= isset($employee) ? $employee['state'] : '' ?>">
         </div>
 
         <div class="col-md-6">
           <label for="uAge" class="form-label">Age</label>
-          <input type="number" class="form-control" id="uAge" name="uAge" required value="<?= isset($employee) ? $employee['age'] : '' ?>">
+          <input type="number" class="form-control" id="uAge" name="age" required value="<?= isset($employee) ? $employee['age'] : '' ?>">
         </div>
 
         <div class="col-md-6">
           <label for="uPostalCode" class="form-label">Postal Code</label>
-          <input type="number" class="form-control" id="uPostalCode" name="uPostalCode" required value="<?= isset($employee) ? $employee['postalCode'] : '' ?>">
+          <input type="number" class="form-control" id="uPostalCode" name="postalCode" required value="<?= isset($employee) ? $employee['postalCode'] : '' ?>">
         </div>
 
         <div class="col-md-6">
           <label for="uPhoneNumber" class="form-label">Phone Number</label>
-          <input type="number" class="form-control" id="uPhoneNumber" name="uPhoneNumber" required value="<?= isset($employee) ? $employee['phoneNumber'] : '' ?>">
+          <input type="number" class="form-control" id="uPhoneNumber" name="phoneNumber" required value="<?= isset($employee) ? $employee['phoneNumber'] : '' ?>">
         </div>
       </div>
 
