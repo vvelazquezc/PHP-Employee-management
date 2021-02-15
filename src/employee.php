dede239b6828e872992ab5f7d900f3c4afe3a30e
iarrieta90 <irati_arrieta90@hotmail.com>
2021-02-15 15:59:45 +0100

    radio buttons styled as images

--- src/employee.php
+++ src/employee.php
@@ -21,9 +21,9 @@ require("../assets/html/header.html");
       $counter = 1;
       foreach ($result as $avatar) {
       echo '<label class="label-avatar" for="avatar' . $counter .'">
+              <input type="radio" id="avatar' . $counter .'" name="avatar" value="' . urldecode($avatar['photo']) . '">
               <img class="img_avatar" src="' . urldecode($avatar['photo']) . '" alt="avatar' . $counter . '">
-            </label>
-            <input type="radio" id="avatar' . $counter .'" name="avatar" value="' . urldecode($avatar['photo']) . '">';
+            </label>';
       $counter ++;
     }?>
     </div>
