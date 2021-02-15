f54b82024ed48d2247d63ad6884e7f38dd0fc6ec
iarrieta90 <irati_arrieta90@hotmail.com>
2021-02-15 16:41:54 +0100

    display current image as selected on avatars carousel

--- src/imageGallery.php
+++ src/imageGallery.php
@@ -3,7 +3,7 @@ require("./library/avatarsApi.php");
 $counter = 1;
 foreach ($result as $avatar) {
     echo '<label class="label-avatar" for="avatar' . $counter . '">
-              <input type="radio" id="avatar' . $counter . '" name="avatar" value="' . urldecode($avatar['photo']) . '">
+              <input type="radio" id="avatar' . $counter . '" class="input_avatar" name="avatar" value="' . urldecode($avatar['photo']) . '" checked="false" >
               <img class="img_avatar" src="' . urldecode($avatar['photo']) . '" alt="avatar' . $counter . '">
             </label>';
     $counter++;
